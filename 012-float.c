65d0199b53fba9706fee2392d1e7aaeb6f81f35b
Abmvk <abmvk@icloud.com>
2023-03-19 22:10:39 +0100

    en de float

---
+++ 012-float.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() 
+{
+    printf("Geheugenbeslag voor een float : %d \n", sizeof(float));
+    printf("FLT_MAX     :   %g\n", (float) FLT_MAX);
+    printf("FLT_MIN     :   %g\n", (float) FLT_MIN);
+    printf("-FLT_MAX    :   %g\n", (float) -FLT_MAX);
+    printf("-FLT_MIN    :   %g\n", (float) -FLT_MIN);
+    printf("DBL_MAX     :   %g\n", (double) DBL_MAX);
+    printf("DBL_MIN     :   %g\n", (double) DBL_MIN);
+    printf("-DBL_MAX     :  %g\n", (double) -DBL_MAX);
+    printf("Nauwkeurigheid: %d\n", FLT_DIG );
+
+    return 0;
+}
