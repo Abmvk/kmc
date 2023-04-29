c665d77817499449703a52662222237381792b3a
Abmvk <126699084+Abmvk@users.noreply.github.com>
2023-04-29 23:41:25 +0200

    test alternatieve functie voor clearscreen

---
+++ ClearScreen.c
@@ -0,0 +1,16 @@
+/* voorstel van ChatGPT ter vervanging van system("clear"). Deze aanpak zou meer platform-onafhankelijk moeten zijn. Nog te testen.
+*/
+
+
+#include <stdio.h>
+
+void clearScreen() {
+    printf("\033[2J"); // Print ANSI escape sequence to clear screen
+    printf("\033[%d;%dH", 0, 0); // Move cursor to top-left corner of screen
+}
+
+int main() {
+    clearScreen(); // Call the function to clear the screen
+    printf("Hello, world!\n"); // Output text
+    return 0;
+}
