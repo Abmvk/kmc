edf07ea1903e8ca054c2426f2830144b48aeb6be
Abmvk <abmvk@icloud.com>
2023-04-30 10:58:57 +0200

    cls() toegevoegd aan stdavk.h

--- stdavk.h
+++ stdavk.h
@@ -1,18 +1,8 @@
 #ifndef STDAVK_H
 #define STDAVK_H
 
-
-/* input-functie, return is 0 of 1 bij een fout. Argumenten zijn:
-vraag: string die voor de input wordt gezet met printf()
-inputs: de string waar de input uit stdin wordt opgeslagen
-lengte: de maximale lengte van inputs
-
-Alle invoer na lengte wordt genegeerd en uit de stdin stream gehaald
-*/
-
-
 int input(char vraag[], char inputs[], int lengte);
 
 void cls();
 
-#endif // STDAVK_H
+#endif
