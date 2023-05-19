1b2061519541c12e2bafcbffbd3166b7b53d5c3d
Abmvk <abmvk@icloud.com>
2023-05-19 23:34:32 +0200

    commentaar toegevoegd

--- 030-life.c
+++ 030-life.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
+/* Conway's Game of Life. Na uitleg van de werking kan de gebruiker een veld tekenen. Vervolgens berekent en tekent het programma een
+aantal nieuwe generaties op basis van de regels van Life. Iedere generatie wordt in beeld gebracht. Dit proces wordt zo vaak herhaald als
+door de gebruiker op de CLI opgegeven, met een default waarde van 1000.
+
+De ncurses bibliotheek wordt gebruikt voor het plaatsen van de cursor op een opgegeven locatie, en voor het weergeven van een blokje voor
+iedere levende cel.  */
+
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
-#include <ctype.h>
-#include <ncurses.h>
-#include "stdavk.h"
+#include <ctype.h>			// nodig om het argument waarmee gestart wordt te testen op numerieke tekens
+#include <ncurses.h>			// ncurses.h maakt stdio.h in dit geval overbodig
+
+#define CELL_VELD 0			// het veld dat wordt weergegeven
+#define COPIE_VELD 1			// een tijdelijk veld voor de berekening van de volgende generatie
 
 
-#define CELL_VELD 0
-#define COPIE_VELD 1
-
-
-struct GameOfLife
-{
-	bool ***veld;
-	int maxX;
-	int maxY;
+struct GameOfLife			// In deze struct geven de functies steeds het te bewerken veld en de maximale omvang door
+{					// het krijgt de vorm game.veld[x][y][CELL_VELD]
+	bool ***veld;			// veld is een drie-dimensionale array van cellen die aan (true) of uit (false) staan
+	int maxX;			// de hoogste waarde van x op het beschikbare scherm of terminal-window
+	int maxY;			// de hoogste waarde van y op het beschikbare scherm of terminal-window
 };
 
+
+
+// De gebruikte functies:
+// is_getal() controleert of het opgegeven argument van main een getal voorstelt
+// cell_set() tekent of wist een cel op de opgegeven locatie, mits passend binnen de scherm-grootte
+// maakVeld() laat de gebruiker een veld tekenen
+// berekenNieuweGeneratie berekent op basis van de regels van Life de volgende generatie van een bestaand veld
 
 int is_getal(const char* str);
 void cell_set(const struct GameOfLife *game, int x, int y, bool aan);
@@ -28,10 +38,11 @@ void berekenNieuweGeneratie(struct GameOfLife *game);
 
 int main(int argc, char **argv)
 {
-	struct GameOfLife game;
-	bool pauze = true;
-	int aantal = 1000, ch;
+	struct GameOfLife game;		// game is de struct met daarin het Life-veld
+	bool pauze = true;		// pauze bepaalt of de volgende generaties berekend worden met of zonder pauze
+	int aantal = 1000, ch;		// aantal is het aantal te berekenen generaties, ch is een hulp-variabele
 
+// als een argument is opgegeven en dit is een getal, dan wordt dat het aantal generaties
 	if(argc > 1 && is_getal(argv[1]))
 		aantal = atoi(argv[1]);
 
@@ -41,11 +52,13 @@ int main(int argc, char **argv)
 	noecho();
 	keypad(stdscr, TRUE);
 	clear();
+
+// bepaal de omvang van het veld op basis van de maximale x- en y-coordinaat van het beschikbare scherm of window
 	getmaxyx(stdscr, game.maxX, game.maxY);
 	game.maxX--;
 	game.maxY--;
 
-// veld initialiseren
+// veld initialiseren: game.veld[x][y][VELD] wordt dynamisch gealloceerd
 	game.veld = malloc((game.maxX+2) * sizeof(bool **));
 	for(int x = 0; x <= game.maxX; x++)
 	{
@@ -77,7 +90,7 @@ int main(int argc, char **argv)
 	maakVeld(&game);
 
 
-// bereken generaties
+// bereken 'aantal' generaties, met pauze, tot gebruiker op f drukt. Linksboven loopt een teller van het aantal generaties dat nog komt
 	do
 	{
 		berekenNieuweGeneratie(&game);
@@ -98,7 +111,7 @@ int main(int argc, char **argv)
 	endwin();
 
 
-// veld vrij geven
+// veld vrij geven, immers met malloc() gealloceerd, dus moet nog free()
 	for(int x = 0; x <= game.maxX; x++)
 	{
 		for(int y = 0; y <= game.maxY+1; y++)
@@ -113,6 +126,8 @@ return 0;
 }
 
 
+// op basis van de aangeleverde string wordt eerst bepaald of deze leeg is of direct afsluit, daarna karakter voor karakter testen of het
+// een cijfer is. Zijn het alleen cijfers, dan is het dus een getal.
 int is_getal(const char* str)
 {
 	if(str == NULL || *str == '\0')
@@ -128,7 +143,8 @@ int is_getal(const char* str)
 return 1;
 }
 
-
+// de game struct wordt alleen meegeleverd zodat getest kan worden of x en y binnen de grenzen vallen. De functie verandert niets aan het
+// veld. Alleen wordt op de gegeven coordinaat op het scherm een blokje geplaatst (spatie met atribuut REVERSE) of gewist (spatie)
 void cell_set(const struct GameOfLife *game, int x, int y, bool aan)
 {
 	if(x<0 || x>game->maxX || y<0 || y>game->maxY)
@@ -140,6 +156,7 @@ void cell_set(const struct GameOfLife *game, int x, int y, bool aan)
 }
 
 
+// de volledige array die het veld beschrijft wordt doorlopen, en per cel wordt de inhoud getekend of gewist, afhankelijk van de status
 void tekenVeld(const struct GameOfLife *game)
 {
 	for(int x=0; x<=game->maxX; x++)
@@ -151,7 +168,13 @@ void tekenVeld(const struct GameOfLife *game)
 	refresh();
 }
 
-
+// hier wordt het veld gevuld en tegelijk getekend. De cursor start in het midden. Zolang de gebruiker niet op enter drukt wordt de loop
+// herhaald. Deze loop bestaat uit het tekenen van het hele veld, het plaatsen van de cursor en deze veranderen op basis van de inhoud
+// van die cel. Het programma moet zonder kleur kunnen werken, dus wordt de status van de cel onder de cursor met een karakter weergegeven
+// vervolgens wordt het toetsenbord uitgelezen in ch. En met een switch wordt per relevante toets de actie bepaalt. Verplaatsen, tekenen,
+// gummen, of geen actie. Hoewel de gebruiker ervaart dat hij "tekent" verandert hij in feite alleen de cellen. Het programma tekent
+// vervolgens steeds het hele veld opnieuw
+// De variabele 'teken' heeft een functie die lijkt op bool, maar kent drie statussen. -1 is wissen, 1 is tekenen, en 0 doet niets.
 void maakVeld(struct GameOfLife *game)
 {
 	int x=game->maxX / 2;
@@ -211,6 +234,13 @@ void maakVeld(struct GameOfLife *game)
 }
 
 
+// Voor iedere cel moet op basis van het aantal levende buren bepaald worden of deze cel tot leven komt, blijft leven, of sterft.
+// Omdat deze regel tegelijk voor alle cellen geldt, en dus niet binnen een generatie effect kan hebben op andere cellen wordt gewerkt
+// met een copie-veld. Hier komt de status in de nieuwe generatie op basis van de buren uit de oude generatie. Als alle cellen zijn
+// gedaan, wordt het hele copie-veld gekopieerd naar het actuele veld en is de nieuwe generatie dus de huidige generatie geworden
+// Een extra complicerende factor is dat een veld volgens de regels onbegrensd moet zijn. Een praktische benadering is het veld ahw
+// als bol te zien. Ga je verder naar links, rechts, boven of onder dan mogelijk, dan kom je aan de andere kant uit.
+// Bij het tellen van het aantal buren mag de cel zelf niet meetellen, dus per cel worden 9 cellen geteld, met uitzondering van de cel zelf
 void berekenNieuweGeneratie(struct GameOfLife *game)
 {
 	int x=0, y=0, buren, i, j, xb, yb;
