f53869ef5de08fb3cb4760b5cfaf831160913aff
Abmvk <abmvk@icloud.com>
2023-05-19 15:22:04 +0200

    tekenfuncties en berekeningen toegevoegd, randen kloppen nog niet

--- life/life.c
+++ life/life.c
@@ -19,7 +19,7 @@ struct GameOfLife
 void cell_draw(int x, int y, bool aan);
 void tekenVeld(const struct GameOfLife *game);
 void maakVeld(struct GameOfLife *game);
-//void berekenNieuweGeneratie(struct GameOfLife *game);
+void berekenNieuweGeneratie(struct GameOfLife *game);
 
 int main(int argc, char **argv)
 {
@@ -28,22 +28,25 @@ int main(int argc, char **argv)
 	initscr();
 	cbreak();
 	noecho();
+	keypad(stdscr, TRUE);
 	struct GameOfLife game;
 	for (x=0; x<=MAX_X; x++)
 		for(y=0; y<=MAX_Y; y++)
-			game.veld[x][y][0] = false;
+			game.veld[x][y][CELL_VELD] = false;
 // scherm klaar voor gebruik
 
 
-
+	maakVeld(&game);
 	tekenVeld(&game);
+	for(int test=0; test<100; test++){
+	berekenNieuweGeneratie(&game);
+	tekenVeld(&game);
+getch();}
 
 
 
 
-
-
-// scherm afsluiten voor einde programma	
+// scherm afsluiten voor einde programma
 	getch();
 	endwin();
 
@@ -76,11 +79,68 @@ void maakVeld(struct GameOfLife *game)
 {
 	int x=MAX_X/2;
 	int y=MAX_Y/2;
+	int ch;
+	bool stoppen = false;
 
+	while(!stoppen)
+	{
+		tekenVeld(game);
+		cell_set(x, y, !game->veld[x][y][CELL_VELD]);
+		cell_set(x, y, game->veld[x][y][CELL_VELD]);
+		ch = getch();
 
+		switch(ch)
+		{
+			case 'q':
+				if(x>0) x--;
+				break;
+			case 'a':
+				if(x<MAX_X) x++;
+				break;
+			case 'o':
+				if(y>0) y--;
+				break;
+			case 'p':
+				if(y<MAX_Y) y++;
+				break;
+			case ' ':
+				game->veld[x][y][CELL_VELD] = !game->veld[x][y][CELL_VELD];
+				break;
+			case '\n':
+				stoppen = true;
+				break;
+			default:
+				break;
+		}
+	}
+}
 
-
-
-
-	
+void berekenNieuweGeneratie(struct GameOfLife *game)
+{
+	int x=0, y=0, buren, i, j, xb, yb;
+	for (x=0; x<=MAX_X; x++)
+		for(y=0; y<=MAX_Y; y++)
+			{
+				buren = 0;
+				for(i=x-1; i<=x+1; i++)
+					for(j=y-1; j<=y+1; j++)
+					{
+						if(i == x && j == y)
+							continue;
+						xb = i<0 ? MAX_X : i;
+						xb = i>MAX_X ? 0 : i;
+						yb = j<0 ? MAX_Y : j;
+						yb = j>MAX_Y ? 0 : j;
+						if(game->veld[xb][yb][CELL_VELD]) buren++;
+					}
+				if(game->veld[x][y][CELL_VELD] && buren == 2)
+					game->veld[x][y][COPIE_VELD] = true;
+				if(buren > 3 || buren < 2)
+					game->veld[x][y][COPIE_VELD] = false;
+				if(buren == 3)
+					game->veld[x][y][COPIE_VELD] = true;
+			}
+	for(x=0; x<=MAX_X; x++)
+		for(y=0; y<=MAX_Y; y++)
+			game->veld[x][y][CELL_VELD] = game->veld[x][y][COPIE_VELD];
 }
